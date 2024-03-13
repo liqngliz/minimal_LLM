2ca241e12c8b14f445e56693bddfd318b263cb7c
liqngliz <liqngliz@gmail.com>
2024-03-13 08:31:58 +0100

    SubPlanner tests detect multiple functions

--- minimal.LLM.SemanticKernel.Test/Planner/SubPlannerFunctionsTests.cs
+++ minimal.LLM.SemanticKernel.Test/Planner/SubPlannerFunctionsTests.cs
@@ -17,9 +17,11 @@ namespace PlannerTests;
 public class SubPlannerFunctionsTest
 {
     private Planner.IPlanner<Task<List<KernelFunction>>, KernelPlan> sut;
-    private IKernelBuilder _builder;
+    readonly IKernelBuilder _builder;
     readonly IModule<Config> _module;
     readonly IFactory<ILLamaExecutor> _factory;
+
+    readonly Kernel _kernel;
 
     public SubPlannerFunctionsTest()
     {   
@@ -27,21 +29,27 @@ public class SubPlannerFunctionsTest
         _module = new IoCModule(configurationJSON);
         var llmContainer = _module.Container();
         _factory = llmContainer.Resolve<IFactory<ILLamaExecutor>>();
-    }
-
-    [Fact]
-    public async void Should_return_functions_for_prompt()
-    {   
         _builder = Kernel.CreateBuilder();
         var reasonerFactory = _module.Container().Resolve<IFactory<IReasoner<Reasoning, ReasonerTemplate>>>();
         _builder.Services.AddKeyedSingleton<IFactory<IReasoner<Reasoning, ReasonerTemplate>>>("local-llama-reasoner-factory", reasonerFactory);
         _builder.Plugins.AddFromType<MathPlugin>();
-        Kernel kernel = _builder.Build();
-        KernelPlan kPlan = new(kernel, "what is the square root of 9");
+        _kernel = _builder.Build();
+    }
+
+    [Theory]
+    [InlineData("what is the square root of 9", "Sqrt")]
+    [InlineData("what is 5 plus 1982", "Add")]
+    [InlineData("876 minus -1", "Subtract")]
+    [InlineData("What is -56 added to 78", "Add")]
+    [InlineData("57 multiplied by 978", "Multiply")]
+    public async void Should_return_functions_for_prompt(string prompt, string function)
+    {   
+        KernelPlan kPlan = new(_kernel, prompt);
         sut = new Planner.Functions.SubPlannerFunctions();
         var plans = await sut.Plan(kPlan);
         var functionNames = plans.Select(x => x.Name);
-        Assert.Equal("Sqrt", plans[0].Name);
+        Assert.Equal(function, plans[0].Name);
         Assert.True(functionNames.Count() == 1);
     }
+
 }
